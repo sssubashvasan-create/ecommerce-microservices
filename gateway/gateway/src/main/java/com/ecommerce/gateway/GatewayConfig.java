ce3557539d963d5ddce70a59900f9f65edba7f9a
SubashVasanSS15 <robinsubash15122000@gmail.com>
2026-09-07 16:21:27 +0530

    retry pattern in gateway

--- gateway/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
@@ -13,7 +14,11 @@ public class GatewayConfig {
         return builder.routes()
                 .route("product-service", r -> r
                         .path("/api/products/**")
-                        .filters(f -> f.circuitBreaker(config -> config
+                        .filters(f -> f.retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setMethods(HttpMethod.GET)
+                                )
+                                .circuitBreaker(config -> config
                                 .setName("ecomBreaker")
                                 .setFallbackUri("forward:/fallback/products")))
 //                        .filters(f -> f.rewritePath("/products(?<segment>/?.*)", "/api/products${segment}"))
--- product/product/src/main/java/com/ecommerce/product/controllers/ProductController.java
+++ product/product/src/main/java/com/ecommerce/product/controllers/ProductController.java
@@ -12,10 +12,18 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping("/simulate")
+    public ResponseEntity<String> simulateFailure(@RequestParam(defaultValue = "false") boolean fail){
+        if(fail){
+            throw new RuntimeException("Simulated Failure For Testing");
+        }
+        return ResponseEntity.ok("Product Service is OK.");
+    }
 
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest){
