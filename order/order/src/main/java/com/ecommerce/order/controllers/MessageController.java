2adf885d53c45f05d5d3d6ecbda70b10f943c4d4
SubashVasanSS15 <robinsubash15122000@gmail.com>
2026-09-08 08:11:14 +0530

    rate limitter fault tolerance config added in method level

--- order/order/src/main/java/com/ecommerce/order/controllers/MessageController.java
+++ order/order/src/main/java/com/ecommerce/order/controllers/MessageController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.order.controllers;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,12 @@ public class MessageController {
     private String message;
 
     @GetMapping("/message")
+    @RateLimiter(name = "orderRateLimiter", fallbackMethod = "getMessageFallBack")
     public String getMessage(){
         return message;
+    }
+
+    public String getMessageFallBack(Exception e){
+        return "Hello Fallback";
     }
 }
